a3862838c78153bd4c459761758061933ef22f95
dkimbi <84943702+dkimbi@users.noreply.github.com>
2021-10-19 23:42:16 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//import * as from "wordArrays";
+
+//document.getElementById("count-el").innerText = 5
+
+
+// column one of words
+const columnFour = [
+    "24/7.", "can I get an amen?", "and that's a fact.", "so treat yourself.", "you feel me?", "that's just science.", "would I lie?","for reals.","mic drop.","you hidden gem.","snuggle bear.","period.","can I get an amen?","now let's dance.",
+    "high five.","say it again!","according to CNN.","so get used to it."
+        ];
+    
+document.getElementById("count-el").innerText = columnFour[Math.random()]
---
+++ wordArrays.js
@@ -0,0 +1,33 @@
+// column one of words
+const columnOne = [
+"Champ", "Fact", "Everybody says", "Dang...", "Check it:", "Just saying...", "Superstar,","Tiger","Self,","Know this:","News alert","Girl,","Ace,","Excuse me but",
+"Experts agree:","In my opinion,","Hear ye, hear ye:","Okay, listen up:"
+    ];
+
+// column one of words
+const columnTwo = [
+"the mere idea of you", "your soul", "your hair today", "everything you do", "your personal style", "every thought you have", "that sparkle in your eye","your presence here","what you got going on","the essential you","your life's journey","that saucy personality","your DNA","that brain of yours",
+"your choice of attire","the way you roll","whatever your secret is","all of y'all"
+    ];
+
+// column one of words
+const columnThree = [
+"has a serious,", "rains magic,", "deserves the Nobel Prize,", "raises the roof,", "breed miracles,", "is paying off big time,", "shows mad skills,","just shimmers,","is national treasure,","gets the party hopping,","the next big thing,","roars like a lion,","is a rainbow factory,","is made of diamonds,","makes birds sing,","should be taught in school,","makes my world go round,","is 100% legit,"
+	];
+
+// column one of words
+const columnFour = [
+"24/7.", "can I get an amen?", "and that's a fact.", "so treat yourself.", "you feel me?", "that's just science.", "would I lie?","for reals.","mic drop.","you hidden gem.","snuggle bear.","period.","can I get an amen?","now let's dance.",
+"high five.","say it again!","according to CNN.","so get used to it."
+	];
+
+index_num1 = Math.floor(Math.random() * 11);
+index_num2 = Math.floor(Math.random() * 11);
+index_num3 = Math.floor(Math.random() * 11);
+index_num4 = Math.floor(Math.random() * 11);
+//console.log(index_num)
+console.log(columnOne[index_num1])
+console.log(columnTwo[index_num2])
+console.log(columnThree[index_num3])
+console.log(columnFour[index_num4])
+// NOT integer console.log(Math.random())
