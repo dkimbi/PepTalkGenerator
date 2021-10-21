d7e5211431f8da5add86db576a5bc8420e890f13
dkimbi <84943702+dkimbi@users.noreply.github.com>
2021-10-21 12:14:40 -0400

    Implimented button and index generator function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 
 // column one of words
-
+*/
 const columnFour = [
     "24/7.", "can I get an amen?", "and that's a fact.", "so treat yourself.", "you feel me?", "that's just science.", "would I lie?","for reals.","mic drop.","you hidden gem.","snuggle bear.","period.","can I get an amen?","now let's dance.",
     "high five.","say it again!","according to CNN.","so get used to it."
@@ -18,18 +18,59 @@ const columnFour = [
 
 // document.querySelector('main').innerHTML = sentence;
 
-// document.querySelector('count-el-dee').innerHTML = `<h3>${nameDee}</h3>`;
+// document.querySelector('word-el').innerHTML = `<h3>${nameDee}</h3>`;
 
+function motivationalSentense(){
+    indexNumberGen = Math.floor(Math.random() * 10)
+    motivationalWord = columnFour[indexNumberGen]
+    console.log(motivationalWord);
+    document.getElementById("word-el").innerText = motivationalWord
+
+}
+
+
+/*
 let myName = "Blue";
 
 alert(`Hi, ${myName}. Want to see something cool?`);
 
 document.querySelector('html').innerHTML = `<h1>Welcome to ${myName}'s site!</h1>`;
 
-*/
+
 
 let count = 5
 
 count = count + 1 
 
 console.log(count)
+
+
+function increment(){
+    console.log("Button clicked!")
+}
+
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+function theAnswer(){
+    console.log(42)
+}
+
+theAnswer()
+*/
+
+count = 0;
+// listen for clicks on the increment button
+function increment(){
+    count++
+    console.log(count)
+    document.getElementById("count-el").innerText = count
+
+}
+
+// increment the count variable when the button is clicked (log it out)
+
+
+// change the count-el in the HTML to reflect the new count
+
+//document.getElementById("count-el").innerText = count
+
