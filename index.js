697bbd665e828db6e567edc97d0cc113d9db5d65
dkimbi <84943702+dkimbi@users.noreply.github.com>
2021-10-20 21:04:06 -0400

    Edite(image and text properties) to html and css files

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+/*
 //import * as from "wordArrays";
 
 //document.getElementById("count-el").innerText = 5
 
 
 // column one of words
+
 const columnFour = [
     "24/7.", "can I get an amen?", "and that's a fact.", "so treat yourself.", "you feel me?", "that's just science.", "would I lie?","for reals.","mic drop.","you hidden gem.","snuggle bear.","period.","can I get an amen?","now let's dance.",
     "high five.","say it again!","according to CNN.","so get used to it."
         ];
     
-document.getElementById("count-el").innerText = columnFour[Math.random()]
+//document.getElementById("count-el").innerText = columnFour[Math.random()]
+// document.querySelector("count-el-dee").innerText = columnFour[Math.random()]
+
+//nameDee = columnFour[Math.random()]
+
+// document.querySelector('main').innerHTML = sentence;
+
+// document.querySelector('count-el-dee').innerHTML = `<h3>${nameDee}</h3>`;
+
+let myName = "Blue";
+
+alert(`Hi, ${myName}. Want to see something cool?`);
+
+document.querySelector('html').innerHTML = `<h1>Welcome to ${myName}'s site!</h1>`;
+
+*/
+
+let count = 5
+
+count = count + 1 
+
+console.log(count)
