f4c0e0946a499679ea815f1c7d88e7881d5aea43
dkimbi <84943702+dkimbi@users.noreply.github.com>
2021-10-25 19:59:10 -0400

    tested modules exp/imp

--- index.js
+++ index.js
@@ -1,50 +1,31 @@
-//import "./wordArrays";
-
-/*
-//document.getElementById("count-el").innerText = 5
+//import { columnOne, columnTwo, columnThree, columnFour } from 'wordArrays';
 
 
-// column one of words
 
-const columnFour = [
-    "24/7.", "can I get an amen?", "and that's a fact.", "so treat yourself.", "you feel me?", "that's just science.", "would I lie?", "for reals.", "mic drop.", "you hidden gem.", "snuggle bear.", "period.", "can I get an amen?", "now let's dance.",
-    "high five.", "say it again!", "according to CNN.", "so get used to it."
-];
 
-*/
-
-//document.getElementById("count-el").innerText = columnFour[Math.random()]
-// document.querySelector("count-el-dee").innerText = columnFour[Math.random()]
-
-//nameDee = columnFour[Math.random()]
-
-// document.querySelector('main').innerHTML = sentence;
-
-// document.querySelector('word-el').innerHTML = `<h3>${nameDee}</h3>`;
-
-// column one of words
 const columnOne = [
     "Champ", "Fact", "Everybody says", "Dang...", "Check it:", "Just saying...", "Superstar,","Tiger","Self,","Know this:","News alert","Girl,","Ace,","Excuse me but",
     "Experts agree:","In my opinion,","Hear ye, hear ye:","Okay, listen up:"
         ];
     
     // column one of words
-    const columnTwo = [
+const columnTwo = [
     "the mere idea of you", "your soul", "your hair today", "everything you do", "your personal style", "every thought you have", "that sparkle in your eye","your presence here","what you got going on","the essential you","your life's journey","that saucy personality","your DNA","that brain of yours",
     "your choice of attire","the way you roll","whatever your secret is","all of y'all"
         ];
     
     // column one of words
-    const columnThree = [
+const columnThree = [
     "has a serious,", "rains magic,", "deserves the Nobel Prize,", "raises the roof,", "breed miracles,", "is paying off big time,", "shows mad skills,","just shimmers,","is a national treasure,","gets the party hopping,","the next big thing,","roars like a lion,","is a rainbow factory,","is made of diamonds,","makes birds sing,","should be taught in school,","makes my world go round,","is 100% legit,"
         ];
     
     // column one of words
-    const columnFour = [
+const columnFour = [
     "24/7.", "can I get an amen?", "and that's a fact.", "so treat yourself.", "you feel me?", "that's just science.", "would I lie?","for reals.","mic drop.","you hidden gem.","snuggle bear.","period.","can I get an amen?","now let's dance.",
     "high five.","say it again!","according to CNN.","so get used to it."
         ];
     
+
 
 function firstWordColumn(){
     indexNumberGenOne = Math.floor(Math.random() * 18)
@@ -118,10 +99,6 @@ function increment(){
     console.log("Button clicked!")
 }
 
-// Create a function (you decide the name) that logs out the number 42 to the console
-// Call/invoke the function
-function theAnswer(){
-    console.log(42)
 }
 
 theAnswer()
@@ -141,5 +118,4 @@ function increment() {
 
 // change the count-el in the HTML to reflect the new count
 
-//document.getElementById("count-el").innerText = count
 
